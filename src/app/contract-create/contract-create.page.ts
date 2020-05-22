30fa8a73c6ccb16801b9d24ffd00e41e27a53821
Arleng2017 <arleng2017@gmail.com>
2020-05-23 01:10:09 +0700

    new form for input data and try to post data to api but it not work;
    -ติดปัญหา เหมือน API พัง ไม่สามารถส่งข้อมูลได้  response 400

--- src/app/contract-create/contract-create.page.ts
+++ src/app/contract-create/contract-create.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { AdminService } from 'src/services/admin.service';
 
 @Component({
   selector: 'app-contract-create',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contract-create.page.scss'],
 })
 export class ContractCreatePage implements OnInit {
-
-  constructor() { }
+  public fg:FormGroup;
+  
+  constructor(private fb:FormBuilder,private adminSvc : AdminService) { 
+    this.fg = this.fb.group({
+      'name':null,
+      'commissionPercent':null,
+      'deliveryPricePerOrder' : null
+    });
+  }
 
   ngOnInit() {
   }
 
+  handleSubmit(){
+    console.log(this.fg.value);
+    if(this.fg.valid){
+     // this.adminSvc.createContractCondition(this.fg.value).then((it:any)=>{});
+    }
+  }
 }
