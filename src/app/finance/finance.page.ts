177b26dbaa6d595f9b735abde371c76bf16ea7aa
tatae <tachapong999.gmail.com>
2020-05-22 23:36:47 +0700

    binding page finance && financeCreate

--- src/app/finance/finance.page.ts
+++ src/app/finance/finance.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { FinanceConfirmPage } from '../finance-confirm/finance-confirm.page';
+import { AdminService } from 'src/services/admin.service';
 
 @Component({
   selector: 'app-finance',
@@ -8,10 +9,17 @@ import { FinanceConfirmPage } from '../finance-confirm/finance-confirm.page';
   styleUrls: ['./finance.page.scss'],
 })
 export class FinancePage implements OnInit {
+  financeInfo$ = Promise.resolve([]);
 
-  constructor(private modalController: ModalController) { }
+  constructor(private modalController: ModalController, private adminSvc : AdminService) { }
 
   ngOnInit() {
+    this.financeInfo$ = this.adminSvc.getFinance();
+    this.financeInfo$.then((it:any)=>{
+      console.log(it);
+    });
+
+
   }
 
   async zzz() {
