f106b29c5fe39a68d2939461d24422f9aaecc3cd
arleng2017 <arleng2017@gmail.com>
2020-05-23 18:58:48 +0700

    get&blinding data to display on page.

--- src/app/biker-detail/biker-detail.page.ts
+++ src/app/biker-detail/biker-detail.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AdminService } from 'src/services/admin.service';
 
 @Component({
   selector: 'app-biker-detail',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BikerDetailPage implements OnInit {
 
-  constructor() { }
+  bikerId:string ;
+  bikerInfo$:Promise<any>;
+  title:string;
+
+  constructor(private route:ActivatedRoute,private adminSvc:AdminService) { }
 
   ngOnInit() {
+    this.route.params.subscribe(param=>{
+      this.bikerId = param['bikerId'];
+      console.log('biker id : '+this.bikerId);
+    });
+
+    this.bikerInfo$ = this.adminSvc.getBikerById(this.bikerId);
+    this.bikerInfo$.then((it:any)=>{
+      this.title = it.name;
+    });
+
+
   }
 
 }
