bd858d6fa09eade740ae98df425376c3ed2afc62
powerofwind <powerofwind10@gmail.com>
2020-06-10 14:26:56 +0700

    add message when api respond erroe

--- src/app/restaurant-profile/restaurant-profile.page.ts
+++ src/app/restaurant-profile/restaurant-profile.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AdminService } from 'src/services/admin.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-restaurant-profile',
@@ -13,7 +14,7 @@ export class RestaurantProfilePage implements OnInit {
   public shopName: string;
   public shopInfo$ = Promise.resolve([]);
 
-  constructor(private activatedRoute: ActivatedRoute, private adminSvc: AdminService) {
+  constructor(private activatedRoute: ActivatedRoute, private adminSvc: AdminService,private alertCtr: AlertController) {
   }
 
   ngOnInit() {
@@ -23,11 +24,28 @@ export class RestaurantProfilePage implements OnInit {
     this.getRestaurantById();
   }
 
+  ionViewWillEnter() {
+    
+  }
+
   getRestaurantById() {
     this.shopInfo$ = this.adminSvc.getRestaurantById(this._idShop);
     this.shopInfo$.then(it => {
       console.log(it);
+    }, async error => {
+      const alert = await this.alertCtr.create({
+        header: 'เกิดข้อผิดพลาด',
+        message: error.error.message,
+        buttons: [{
+          text: 'ตกลง',
+          handler: () => {
+            // DO SOMETHING
+          },
+        }],
+        backdropDismiss: false
+      });
 
-    })
+      await alert.present();
+    });
   }
 }
