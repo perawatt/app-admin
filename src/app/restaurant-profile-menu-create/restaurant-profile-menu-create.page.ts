15401ae9e3ff6d8bb9729c93edbb750dcbdf1f7c
woraput <woraput@kkumail.com>
2020-06-17 16:55:46 +0700

    recode checkdisabled

--- src/app/restaurant-profile-menu-create/restaurant-profile-menu-create.page.ts
+++ src/app/restaurant-profile-menu-create/restaurant-profile-menu-create.page.ts
@@ -67,7 +67,9 @@ export class RestaurantProfileMenuCreatePage implements OnInit {
   }
 
   checkCategoryCanNote(): boolean {
-    return this.catagory && this.catagory.find((it: any) => it.categoryName == this.fg.get('categoryName').value).canNote;
+    let categorySelect = this.catagory && this.catagory.find((it: any) => it.categoryName == this.fg.get('categoryName').value);
+
+    return categorySelect && categorySelect.canNote;
   }
 
   selectPhoto(event) {
