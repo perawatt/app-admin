bf1199b00ee236f4d77658d926ad078a68306470
perawatt <pekung4433@perfenterprise.com>
2020-06-19 10:34:28 +0700

    fixed adminId

--- src/providers/adminInfo/admin-info.service.ts
+++ src/providers/adminInfo/admin-info.service.ts
@@ -1,5 +1,5 @@
 export class AdminInfo {
-  public static adminId: string = "1";
+  public static adminId: string = "637263406341106580";
   public static deliveryId: string = "1";
   public static adminName: string;
   public static adminProfileUrl: string;
