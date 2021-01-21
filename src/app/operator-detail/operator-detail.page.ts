831ebf32f6bb2da896e2c18ba55c64f279301cb6
sikarin <so.sikarin@perfenterprise.com>
2021-01-21 11:10:37 +0700

    change api

--- src/app/operator-detail/operator-detail.page.ts
+++ src/app/operator-detail/operator-detail.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AdminService } from 'src/services/admin.service';
 import { AlertController, NavController } from '@ionic/angular';
+import { log } from 'console';
 
 @Component({
   selector: 'app-operator-detail',
@@ -11,16 +12,18 @@ import { AlertController, NavController } from '@ionic/angular';
 export class OperatorDetailPage implements OnInit {
 
   adminDetail$ = Promise.resolve([]);
-  title:string;
-  // public _id: string;
-  constructor(private navCtrl:NavController,private activatedRoute: ActivatedRoute, private adminSvc: AdminService,private alertCtrl:AlertController) {}
-
-   
-  ngOnInit() {
-   this.loadData();
+  title: string;
+  public id: string;
+  constructor(private navCtrl: NavController, private activatedRoute: ActivatedRoute, private adminSvc: AdminService, private alertCtrl: AlertController) {
+    this.id = this.activatedRoute.snapshot.paramMap.get('_id');
   }
 
-  async loadData(){
+
+  ngOnInit() {
+    this.loadData();
+  }
+
+  async loadData() {
     const alert = await this.alertCtrl.create({
       header: 'เกิดข้อผิดพลาด',
       message: "",
@@ -32,8 +35,9 @@ export class OperatorDetailPage implements OnInit {
       }],
       backdropDismiss: false
     });
-    this.adminDetail$ = this.adminSvc.getAdminById();
-    this.adminDetail$.then((it:any)=>{
+
+    this.adminDetail$ = this.adminSvc.getAdminById(this.id);
+    this.adminDetail$.then((it: any) => {
       this.title = it.name;
     }, async error => {
       alert.message = error.error.message;
